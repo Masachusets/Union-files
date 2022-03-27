29c394e8e6583c7e846f887fea15fd236efab46e
Masachusets <Apofeoz@tut.by>
2022-03-27 21:09:04 +0300

    added package_file

--- Union.py
+++ Union.py
@@ -5,6 +5,14 @@ def unpackage_file(file: str) -> list:
         data.insert(0, file)
     return data
 
-def package_file(*data: list) -> file:
 
-#print(unpackage_file('1.txt'))
+def package_file(file: str, *datas):
+    with open(file, 'w', encoding='utf-8') as f:
+        for data in sorted(datas, key=lambda x: x[1]):
+            f.write(f'{data.pop(0)}\n')
+            f.write(f'{data.pop(0)}\n')
+            for line in data:
+                f.write(f'{line}')
+            f.write('\n')
+
+# package_file('e:/union.txt', unpackage_file('1.txt'), unpackage_file('2.txt'), unpackage_file('3.txt'))
